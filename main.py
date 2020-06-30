2480ec0bfb176dc6b342b83e82124f018bdc1808
Charlie-Ren <540585507qq@gmail.com>
2020-06-29 22:39:30 -0500

    修正了pmi的计算方法

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from read_csv import read_csv
 import logging
-from pmi_process import get_frequency,get_all_words
+import argparse
+from pmi_process import get_frequency,get_all_frequency
 from csv_writer import csv_writer
+import csv
+import synonyms
 logging.basicConfig(level=logging.DEBUG)
 path = '/Users/charilie/Desktop/PMI/l1.csv'
 if __name__ == '__main__':
-    labelset=read_csv(path)
-    logging.debug(labelset)
-    dict_all = get_all_words(path) #所有词的字典
-    res_list = get_frequency(path, '疑似财务风险', dict_all)
-    csv_writer('pmi_dict.csv',res_list,'疑似财务风险')
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--dict",dest="dict",action="store_true",default=False)
+    parser.add_argument("--enlarge", dest="enlarge", action="store_true", default=False)
+    args=parser.parse_args()
+    if args.dict:
+        labelset = read_csv(path)
+        logging.debug(labelset)
+        dict_all = get_all_frequency(path)  # 所有词的字典
+        for label in labelset:
+            res_list = get_frequency(path, label, dict_all)
+            csv_writer('pmi_dict_l1.csv', res_list, label)
+    if args.enlarge:
+        dict_path="pmi_dict_l1.csv"
+        output_path="l1_enlarged_dict.csv"
+        with open(dict_path,'r',encoding='utf-8-sig') as csvfile: #完美解决ufeff
+            reader=csv.DictReader(csvfile)
+            for line in reader:
+                word=line['word']
+                word_label=line['label']
+                word_list=synonyms.nearby(word)[0]
+                csv_writer(output_path,word_list,word_label)#把每个同义词都写进去
+
+
 
--- pmi_process.py
+++ pmi_process.py
@@ -1,28 +1,30 @@
 import jieba
 import csv
 import math
-import numpy as np
 
-def get_all_words(path):
+def get_all_frequency(path): #取出这个词在所有的类
+    all_count = 0 #所有新闻的数量
     with open(path, 'r') as csvfile:
         dict_all = {}
         reader = csv.DictReader(csvfile)
         for line in reader:
+            all_count+=1 #计算出所有新闻的数量.
             sentence = line['text']
-            word_list = list(jieba.cut(sentence,cut_all=True))
-            for word in word_list:  # 先读取所有的字典并取出数值
+            word_list_temp = list(jieba.cut(sentence))#没去过重.
+            word_list=list(set(word_list_temp))#去重过后.
+            for word in word_list:  #先读取所有的字典并取出数值
                 if word in dict_all:
                     dict_all[word] += 1
                 else:
                     temp_dict = {word: 1}
                     dict_all.update(temp_dict)
-    all_count = sum(dict_all.values())
     for key in dict_all:
         dict_all[key] = dict_all[key] / all_count  # 词出现的频率
     return dict_all
 
 
 def get_frequency(path, label,dict_all):  # 根据label来算pmi的值
+    label_count=0
     with open(path, 'r') as csvfile:
         label_dict = {}
         reader = csv.DictReader(csvfile)
@@ -30,33 +32,31 @@ def get_frequency(path, label,dict_all):  # 根据label来算pmi的值
             if line['label2'] != label:
                 continue
             else:
+                label_count+=1#计算这个标签中的数量
                 sentence=line['text']
-                word_list=list(jieba.cut(sentence,cut_all=True))
+                word_list_temp=list(jieba.cut(sentence))
+                word_list=list(set(word_list_temp))
                 for word in word_list:
                     if word in label_dict:
                         label_dict[word]+=1
                     else:
                         temp_dict={word:1}
                         label_dict.update(temp_dict)
-                all_count=sum(label_dict.values())
                 for key in label_dict:
                     temp_value=label_dict[key]
-                    temp_value/=all_count
+                    temp_value/=label_count #这类新闻下出现的概率
                     prob=dict_all[key]
-                    temp_value/=prob #这个类下的概率除全部的概率
+                    temp_value/=prob #这个类下的概率除全部新闻的概率
                     if(temp_value<=0): #排除所有为0的
                         label_dict[key]=-100
                         continue
                     temp_value=max((math.log(temp_value,2)),0)
                     label_dict[key]=temp_value
         res=sorted(label_dict.items(),key=lambda item:item[1],reverse=True)#按互信息大小排序, 此时是list
-        res=res[:500]#每个取前500个词
+        res=res[:int(len(res)*0.2)]#每个取前20%个词
         res_dict={}
         for l in res:
             res_dict[l[0]]=l[1]#再转为字典
         res_list=list(res_dict.keys())#只需要词,不需要keyword.
         print(res_list)
         return res_list
-#dict_all=get_all_words('/Users/charilie/Desktop/PMI/l1.csv')
-#res_dict=get_frequency('/Users/charilie/Desktop/PMI/l1.csv','疑似财务风险',dict_all)
-
